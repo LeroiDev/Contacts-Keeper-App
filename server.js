535e5fed41d423d064a7082948f0bc216836b73e
Jen-kins <60917179+Jen-kins@users.noreply.github.com>
2020-09-30 15:39:24 +0200

    fixed-routes-setup-express-server-in-progress

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/users"));
-app.use("./api/contacts", require("./routes/contacts"));
-app.use("./api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
